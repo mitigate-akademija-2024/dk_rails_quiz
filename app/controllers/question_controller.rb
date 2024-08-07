205e0aacfa49116e6ea3d5371f94b8128b088217
DKalnbunde <dainis.kalnbunde@gmail.com>
2024-08-07 13:57:20 +0000

    JSON response

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,13 @@
+class QuestionController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def start
+  end
+
+  def test
+  end
+end
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,6 +4,21 @@ class QuizzesController < ApplicationController
   # GET /quizzes or /quizzes.json
   def index
     @quizzes = Quiz.all
+
+    @title = "These are the quizes"
+    @description = "lorem 150"
+  end
+
+  def start
+    @title = "Start some new quizes"
+    @description = "lorem 250"
+
+    respond_to do |format|
+      format.html
+      format.json do 
+        render json: {title: "new title", description: "šī ir json atbilde"}
+      end
+    end
   end
 
   # GET /quizzes/1 or /quizzes/1.json
